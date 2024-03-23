49c8170a715dac94d94c1dd12da59ce395e7a455
kexogg <kexogg@gmail.com>
2024-03-24 00:19:46 +0500

    data exchange

--- EditingService/Api/Models/GetCourseResponse.cs
+++ EditingService/Api/Models/GetCourseResponse.cs
@@ -7,4 +7,13 @@ public record GetCourseResponse
     public required string Name { get; init; }
     
     public required string Description { get; init; }
+    
+    public required Participant[] Participants { get; init; }
+}
+
+public abstract record Participant
+{
+    public required Guid Id { get; init; }
+    
+    public required string Name { get; init; }
 }
